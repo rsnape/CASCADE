a784a35ebbf4c9df0381605b8dae41a2a23eb239
rsnape <rsnape@dmu.ac.uk>
2014-11-26 15:54:39 +0000

    Bring Cranfield market into line with other project code format
    Signed-off-by: rsnape <rsnape@dmu.ac.uk>

--- src/uk/ac/cranfield/cascade/market/MarketRecord.java
+++ src/uk/ac/cranfield/cascade/market/MarketRecord.java
@@ -1,6 +1,7 @@
 package uk.ac.cranfield.cascade.market;
 
-public class MarketRecord {
+public class MarketRecord
+{
 	double quantityTraded = 3.0;
 	double moneySpent = 3.0;
 	int tickSize = 0;
@@ -9,34 +10,36 @@ public class MarketRecord {
 	double lastBuyOfferPrice = 0.1;
 	double lastSellOfferPrice = 25;
 	double lastTradeInstantPrice = 0.0;
-	
+
 	void logSale(double price)
 	{
-		if(price > lastTradePrice * 1.5) price = lastTradePrice * 1.5;
-		if(price < lastTradePrice * 0.5) price = lastTradePrice * 0.5;
-		
-		lastTradePrice = 0.9*lastTradePrice + 0.1 * price;
+		if (price > lastTradePrice * 1.5)
+			price = lastTradePrice * 1.5;
+		if (price < lastTradePrice * 0.5)
+			price = lastTradePrice * 0.5;
+
+		lastTradePrice = 0.9 * lastTradePrice + 0.1 * price;
 		lastTradeInstantPrice = price;
 	}
-	
+
 	public MarketRecord clone()
 	{
 		MarketRecord nmr = new MarketRecord();
 		nmr.quantityTraded = quantityTraded;
 		nmr.moneySpent = moneySpent;
 		nmr.tickSize = tickSize;
-		
-		nmr.lastTradePrice=lastTradePrice;
-		nmr.lastBuyOfferPrice=lastBuyOfferPrice;
-		nmr.lastSellOfferPrice=lastSellOfferPrice;
-		
+
+		nmr.lastTradePrice = lastTradePrice;
+		nmr.lastBuyOfferPrice = lastBuyOfferPrice;
+		nmr.lastSellOfferPrice = lastSellOfferPrice;
+
 		return nmr;
 	}
-	
+
 	public void log(int time)
 	{
-		//logs the data about a time tick when we are done;
-		System.out.println("LOG:Time= "+time+" price="+lastTradePrice);
+		// logs the data about a time tick when we are done;
+		System.out.println("LOG:Time= " + time + " price=" + lastTradePrice);
 	}
-	
+
 }
--- src/uk/ac/cranfield/cascade/market/Sync.java
+++ src/uk/ac/cranfield/cascade/market/Sync.java
@@ -1,42 +1,47 @@
 package uk.ac.cranfield.cascade.market;
 
-public class Sync {
+public class Sync
+{
 	int c = 0;
-	
+
 	public Sync(int c)
 	{
 		this.c = c;
 	}
-	
+
 	public synchronized void inc()
 	{
 		c++;
 	}
-	
+
 	public synchronized void dec()
 	{
-		if(c > 0)
-		   c--;
-		if(c==0) this.notifyAll();
+		if (c > 0)
+			c--;
+		if (c == 0)
+			this.notifyAll();
 	}
-	
+
 	public synchronized boolean isZero()
 	{
-		if (c==0) return true;
-		else return false;
+		if (c == 0)
+			return true;
+		else
+			return false;
 	}
-	
+
 	public synchronized void waitZero()
 	{
-		while(!isZero())
+		while (!isZero())
 		{
-			try {
+			try
+			{
 				this.wait();
-			} catch (InterruptedException e) {
+			}
+			catch (InterruptedException e)
+			{
 			}
 		}
 	}
-	
-	
 
 }
